fa55b70e9c84933eb1cc945c592ba06f7ba56b33
RybalkinAN <alexey.rybalkin@caresyntax.com>
2021-09-04 13:54:21 +0400

    fix models

--- src/main/java/com/github/rybalkinan/event/models/Event.java
+++ src/main/java/com/github/rybalkinan/event/models/Event.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -17,11 +18,23 @@ import javax.persistence.Table;
 public class Event extends BaseEntity{
 
     private int organizer;
+
+    @Column(columnDefinition = "event_name")
     private String eventName;
+
+    @Column(columnDefinition = "event_date")
     private String eventDate;
+
+    @Column(columnDefinition = "event_date_range")
     private String eventDateRange;
+
+    @Column(columnDefinition = "event_address")
     private String eventAddress;
+
+    @Column(columnDefinition = "event_type")
     private int eventType;
+
+    @Column(columnDefinition = "event_image")
     private String eventImage;
 
 }
--- src/main/java/com/github/rybalkinan/event/models/EventType.java
+++ src/main/java/com/github/rybalkinan/event/models/EventType.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
@@ -16,6 +17,7 @@ import javax.persistence.Table;
 @AllArgsConstructor
 public class EventType extends BaseEntity{
 
+    @Column(columnDefinition = "event_type_description")
     public String eventTypeDescription;
 
 }
